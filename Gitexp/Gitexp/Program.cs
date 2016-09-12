8b2ee5df87c21f697e022de1a6c5a33b41b7eb37
Blagovest Razhenkov <razhenkov@gmail.com>
2016-09-12 22:55:05 +0300

    Added Gitexp project

---
+++ Gitexp/Gitexp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gitexp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var history = new List<string>();
+            history.Add("Initialize history list");
+
+            history.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
